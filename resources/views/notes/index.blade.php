30f01900942e58cf6ea819f0d956f24e36eeb62a
Vipurthanan <lessandsass@gmail.com>
2024-12-29 13:52:10 +0530

    Retrieving notes from the blade file
    - The notes was retrieved and saves as a notes variables.
    - Then the notes was iterated via the foreach loop.
    - The notes\index.blade.php
    - The note table is empty right now so some records have to be added
    - The Tinker shell was used to create database records.

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -3,13 +3,23 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Notes') }}
         </h2>
-</x-slot>
+    </x-slot>
+
+    @php
+        $notes = App\Models\Note::all()->where('user_id', Auth::user()->id);
+    @endphp
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <x-button primary label="Hi there notes" />
+
+                    @foreach ($notes as $note)
+                        <li>
+                            <p>{{ $note->title }}</p>
+                        </li>
+                    @endforeach
+
                 </div>
             </div>
         </div>
