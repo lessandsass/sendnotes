4363773f3ce1c9892d9e376035987f75e48124af
Vipu <lessandsass@gmail.com>
2025-01-25 20:17:27 +0530

    remove the fly files
    - to restart the deployment

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -19,19 +19,31 @@ new class extends Component {
 <div>
 
     <div class="space-y-2">
-        @foreach ($notes as $note)
-            <x-card wire:key='{{ $note->id }}'>
-                <div class="flex justify-between">
-                    <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500">
-                        {{ $note->title }}
-                    </a>
+        <div class="grid grid-cols-2 gap-4 mt-12">
+            @foreach ($notes as $note)
+                <x-card wire:key='{{ $note->id }}'>
+                    <div class="flex justify-between">
+                        <a href="#" class="text-xl font-bold hover:underline hover:text-blue-500">
+                            {{ $note->title }}
+                        </a>
 
-                    <div class="text-xs text-gray-500">
-                        {{ \Carbon\Carbon::parse($note->send_date)->format('d-M-Y') }}
+                        <div class="text-xs text-gray-500">
+                            {{ \Carbon\Carbon::parse($note->send_date)->format('d-M-Y') }}
+                        </div>
                     </div>
-                </div>
-            </x-card>
-        @endforeach
+
+                    <div
+                        class="flex items-end justify-between mt-4 space-x-1"
+                    >
+                        <p
+                            class="text-xs text-gray-500"
+                        >
+                            {{ $note->created_at->diffForHumans() }}
+                        </p>
+                    </div>
+                </x-card>
+            @endforeach
+        </div>
     </div>
 
 </div>
