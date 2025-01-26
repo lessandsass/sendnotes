6c7f9181109d7dc89f9e1bd6e946d0852c2ef5d2
Vipu <lessandsass@gmail.com>
2025-01-26 14:29:30 +0530

    die and dumping the inputs
    - The form fields were created
    - wire:submit was added, we don't have to add the prevent method because livewire will handle that for you.

--- resources/views/livewire/notes/create-note.blade.php
+++ resources/views/livewire/notes/create-note.blade.php
@@ -18,5 +18,13 @@ new class extends Component {
 ?>
 
 <div>
-    I am going to create a note
+    <div class="w-1/2 mx-auto">
+        <form action="" wire:submit="submit">
+            <x-input icon="pencil" wire:model="noteTitle" label="Note Title" class="mt-5" placeholder="It's been a great day." />
+            <x-textarea wire:model="noteBody" label="Note Body" class="mt-5" rows="3" cols="auto" placeholder="Say additional things about your note." />
+            <x-input icon="user" wire:model="noteRecipient" label="Note Recipient" class="mt-5" placeholder="Your friend email address" type="email" />
+            <x-input icon="calendar" wire:model="noteSendDate" type="date" label="Note Send Date" class="mt-5" />
+            <x-button wire:click="submit" class="mt-5" spinner>Schedule Note</x-button>
+        </form>
+    </div>
 </div>
