2a13182cc6d0c3cc62d55eb2ac82f5edc63618af
Vipu <lessandsass@gmail.com>
2025-02-04 17:33:05 +0530

    Update the note
    - Creating the state
    - The field was mounted to be filled.
    - The saveNote function was created to validate and save the updated input into database.
    - The wire:click was changed to the type submit button, because we are using wire:submit attribute in the form tag
    - The above changes had been done in the edit-note and create-note Volt components
    - The form fields were added
    - The syntax of the edit-note volt component
    - The error was occurred while saving the database.
    - The is_published was casted in the Note class

--- resources/views/livewire/notes/create-note.blade.php
+++ resources/views/livewire/notes/create-note.blade.php
@@ -3,6 +3,7 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
+
     public $noteTitle;
     public $noteBody;
     public $noteRecipient;
@@ -36,10 +37,14 @@ new class extends Component {
     <div class="w-1/2 mx-auto">
         <form action="" wire:submit="submit">
             <x-input icon="pencil" wire:model="noteTitle" label="Note Title" class="mt-5" placeholder="It's been a great day." />
+
             <x-textarea wire:model="noteBody" label="Note Body" class="mt-5" rows="3" cols="auto" placeholder="Say additional things about your note." />
+
             <x-input icon="user" wire:model="noteRecipient" label="Note Recipient" class="mt-5" placeholder="Your friend email address" type="email" />
+
             <x-input icon="calendar" wire:model="noteSendDate" type="date" label="Note Send Date" class="mt-5" />
-            <x-button wire:click="submit" class="mt-5" spinner>Schedule Note</x-button>
+
+            <x-button type="submit" class="mt-5" spinner>Schedule Note</x-button>
         </form>
     </div>
 </div>
--- resources/views/livewire/notes/edit-note.blade.php
+++ resources/views/livewire/notes/edit-note.blade.php
@@ -9,14 +9,48 @@ class extends Component {
 
     public Note $note;
 
+    public $noteTitle;
+    public $noteBody;
+    public $noteRecipient;
+    public $noteSendDate;
+    public $noteIsPublished;
+
     public function mount(Note $note): void
     {
         $this->authorize('update', $note);
-        // $this->note = $note;
         $this->fill($note);
+
+        $this->noteTitle = $note->title;
+        $this->noteBody = $note->body;
+        $this->noteRecipient = $note->recipient;
+        $this->noteSendDate = $note->send_date;
+        $this->noteIsPublished = $note->is_published;
     }
 
-}; ?>
+    public function saveNote()
+    {
+        $validated = $this->validate([
+            'noteTitle' => ['required', 'string', 'max:255', 'min:3'],
+            'noteBody' => ['required', 'string', 'max:255', 'min:10'],
+            'noteRecipient' => ['required', 'email', 'max:255'],
+            'noteSendDate' => ['required', 'date'],
+            'noteIsPublished' => ['required', 'boolean'],
+        ]);
+
+        $this->note->update([
+            'title' => $validated['noteTitle'],
+            'body' => $validated['noteBody'],
+            'recipient' => $validated['noteRecipient'],
+            'send_date' => $validated['noteSendDate'],
+            'is_published' => $validated['noteIsPublished'],
+        ]);
+
+        // redirect(route('notes.index'));
+    }
+
+};
+
+?>
 
 <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -26,10 +60,18 @@ class extends Component {
 
 <div class="py-12">
     <div class="max-w-2xl mx-auto space-y-4 sm:px-6 lg:px-8">
-        <form action="">
+        <form action="" wire:submit="saveNote">
+            <x-input icon="pencil" wire:model="noteTitle" label="Note Title" class="mt-5" placeholder="It's been a great day." />
 
-            My form
+            <x-textarea wire:model="noteBody" label="Note Body" class="mt-5" rows="3" cols="auto" placeholder="Say additional things about your note." />
 
+            <x-input icon="user" wire:model="noteRecipient" label="Note Recipient" class="mt-5" placeholder="Your friend email address" type="email" />
+
+            <x-input icon="calendar" wire:model="noteSendDate" type="date" label="Note Send Date" class="mt-5" />
+
+            <x-checkbox wire:model="noteIsPublished" label="Publish Note" class="mt-5" />
+
+            <x-button type="submit" class="mt-5" spinner>Save Note</x-button>
         </form>
     </div>
 </div>
